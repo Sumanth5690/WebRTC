0b8498a8822209a3768d2d7ce99319748987ee80
sumanth <sumanthsumanthacharya8@gmail.com>
2025-06-08 21:56:22 +0530

    agora setup

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+let APP_ID="6ed42a69b217408893165dfadd853878";
+
+let token=null;
+let uid=String(Math.floor(Math.random()*10000))
+
+let client
+let channel
+
+
 let localStream;
 let remoteStream;
 let peerConnection;
+
+const servers={
+    iceServers:[
+        {
+            urls:['stun:stun1.l.google.com.19302','stun:stun2.l.google.com.19302']
+        }
+    ]
+}
 
 let init=async()=>{
     //getting acess to the streaming from the user 
@@ -11,15 +28,31 @@ createOffer()
 
 
 //creating offer to another video
-let createOffer=async()=>{
-peerConnection=new RTCPeerConnection()
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection(servers);
 
-remoteStream=new MediaStream()
-document.getElementById('user-2').srcObject=remoteStream
+    remoteStream = new MediaStream();
+    document.getElementById('user-2').srcObject = remoteStream;
 
-let offer=await peerConnection.createOffer()
-await peerConnection.setLocalDescription(offer)
-console.log('offer:',offer)
-}
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track);
+        });
+    };
+
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate) {
+            console.log('New ice candidate', event.candidate);
+        }
+    };
+
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    console.log('offer:', offer);
+};
 
 init()
